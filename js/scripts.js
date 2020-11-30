844b493c9ccdd930c07044ee14797239a7acf713
Demarrage <gui.vbsamolao@hotmail.com>
2020-11-30 10:53:19 -0300

    criamos a pagina atualiazar e o script para atualizar os clientes

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ var telefone = document.getElementById("txttelefone");
 var cpf = document.getElementById("txtcpf");
 var idade = document.getElementById("txtidade");
 var btn = document.getElementById("btnCadastrar");
+var bta = document.getElementById("btnAtualizar");
 
 btn.onclick = function () {
   fetch("http://localhost:4505/cadastro", {
@@ -39,25 +40,25 @@ function listarClientes() {
           "<tr>" +
           "<td>" +
           rs.saida[i]._id +
-          "<td>" +
+          "</td>" +
           "<td>" +
           rs.saida[i].nomecliente +
-          "<td>" +
+          "</td>" +
           "<td>" +
           rs.saida[i].email +
-          "<td>" +
+          "</td>" +
           "<td>" +
           rs.saida[i].telefone +
-          "<td>" +
+          "</td>" +
           "<td>" +
           rs.saida[i].cpf +
-          "<td>" +
+          "</td>" +
           "<td>" +
           rs.saida[i].idade +
-          "<td>" +
+          "</td>" +
           "<td>" +
           rs.saida[i].datacadastro +
-          "<td>" +
+          "</td>" +
           '<td><a href="atualizar.html?id=' +
           rs.saida[i]._id +
           '"><i class="material-icons" style="cursor:pointer"> refresh </i> </a> </td>' +
@@ -84,4 +85,48 @@ function apagar(id) {
       })
       .catch((erro) => console.error(`Erro ao tentar a pagar ${erro}`));
   }
+}
+function atualizar() {
+  var ds = window.location.search;
+  var qtd = window.location.search.length;
+  var rs = window.location.search.substring(4, qtd);
+  fetch("http://localhost:4505/atualizar/" + rs, {
+    method: "PUT",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      nomecliente: nome.value,
+      email: email.value,
+      telefone: telefone.value,
+      cpf: cpf.value,
+      idade: idade.value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((rs) => {
+      alert("dados atualizados com sucesso!");
+      window.location.reload();
+    })
+    .catch((erro) => console.error(`erro ao tentar atualizar ${erro}`));
+}
+function carregar() {
+  var qtd = window.location.search.length; //quantidade de caracter
+  var rs = window.location.search.substring(4, qtd);
+  fetch("http://localhost:4505/" + rs)
+    .then((response) => response.json())
+    .then((resultado) => {
+      nome.value = resultado.re.nomecliente;
+      nome.focus();
+      email.value = resultado.re.email;
+      email.focus();
+      telefone.value = resultado.re.telefone;
+      telefone.focus();
+      cpf.value = resultado.re.cpf;
+      cpf.focus();
+      idade.value = resultado.re.idade;
+      idade.focus();
+    })
+    .catch((erro) => console.error(`Erro ao tentar carregar os dados ${erro}`));
 }
